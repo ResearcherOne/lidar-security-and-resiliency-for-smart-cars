95601d4d6556101bc911cc5306377b071e6c869c
Birkan Kolcu <birkan.kolcu@ozu.edu.tr>
2018-01-16 15:35:34 +0300

    created LIDAR_data_structures for standardizing programs accross different LIDARs

--- rplidar/sdk/app/object_detection_program/LIDAR_data_structures.hpp
+++ rplidar/sdk/app/object_detection_program/LIDAR_data_structures.hpp
@@ -1,16 +1,43 @@
 #ifndef LIDAR_DATA_STRUCTURES_H
 #define LIDAR_DATA_STRUCTURES_H
 
-struct LIDAR_data_point {
+enum LIDAR_Directions
+{
+	North, //North is assumed to be where LIDAR is heading by default.
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest
+};
+
+enum LIDAR_Zones
+{
+	first_zone,		//NNE -> North, NorthEast
+	second_zone, 	//ENE -> East, NorthEast
+	third_zone,		//ESE -> East, SouthEast
+	fourth_zone,	//SSE -> South, SourthEast
+	fifth_zone,		//SSW
+	sixth_zone,		//WSW
+	seventh_zone,	//WNW
+	eight_zone		//NNW
+}
+
+struct LIDAR_data_point
+{
 	float thetha;				//0-360
 	float distance;				//cm or what?
 	int measurement_quality; 	//0-100, is unreliable 100 is perfect
 };
 
-struct LIDAR_batch_scan_data {
+struct LIDAR_batch_scan_data 
+{
 	long long int timestamp_msec;
-	//LIDAR_ID
-	//LIDAR data point array
+	int scanned_data_count;
+	int lidar_ID;
+	LIDAR_data_point * LIDAR_data_point_array_ptr;
 };
 
 #endif
